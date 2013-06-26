d15ffe7aee36c246ac5a662b3a421677fe7256b5
Mikolaj Kowalik <mxk62@psu.edu>
2013-06-26 11:30:41 -0400

    Simplify control flow in make_retrostep()
    Method make_retrostep() of Chemical class was returning a set of reaction
    SMILES or None, if applying transform resulted in empty product list. I rewrite
    it to make it return just an empty set in case of failure. It allowed to
    simplify a bit of control flow and should have no unwanted side-effects..

--- src/chemical.py
+++ src/chemical.py
@@ -6,33 +6,29 @@ class Chemical:
     """Represents a chemical compound."""
 
     def __init__(self, smiles):
-        self.smi = smiles.strip()
+        self.smiles = smiles.strip()
         try:
-            self.mol = Chem.MolFromSmiles(self.smi)
+            self.mol = Chem.MolFromSmiles(self.smiles)
         except Exception:
-            print 'Error: invalid compound SMILES: {}.'.format(self.smi)
+            print 'Error: invalid compound SMILES: {}.'.format(self.smiles)
             sys.exit(1)
 
     def make_retrostep(self, transform):
         """Returns unique reactions obtained by retrosynthesis."""
 
-        rxns = set([])
         product_list = []
-
         try:
             product_list = transform.formula.RunReactants((self.mol,))
-        except Exception as error:
+        except Exception:
             # For now, ignore silently any errors
-            #print 'Error: applying transform failed:', error
             pass
 
+        rxn_smis = set([])
         if product_list:
             for products in product_list:
                 reactant_smis = '.'.join(Chem.MolToSmiles(p) for p in products)
-            rxns.add(reactant_smis + '>>' + self.smi)
-        else:
-            return None
-        return rxns
+            rxn_smis.add(reactant_smis + '>>' + self.smiles)
+        return rxn_smis
 
 
 if __name__ == '__main__':
