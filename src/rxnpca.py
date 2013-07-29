420e226e71c176e5a631a48963eb4d38349bd701
Mikolaj Kowalik <mxk62@psu.edu>
2013-07-29 18:35:58 -0400

    Allow to use functional group count as descriptor
    I made the changes to make possible using the functional group count vector as
    a reaction descriptor. Also extended section doing actual PCA to make each step
    more explicit.

--- src/rxnpca.py
+++ src/rxnpca.py
@@ -12,6 +12,7 @@ import mdp
 from numpy import array
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
+from rdkit import Chem
 from sample import Sample
 from reaction import Reaction, Transform
 from chemical import Chemical
@@ -25,6 +26,18 @@ def save_array(a, filename):
                 f.write(str(e) + ' ')
             f.write('\n')
 
+
+# Create a map between functional group SMARTS and their representation as
+# query molecules (used in RDKit's HasSubstructMatch() function).
+groups = {}
+with open('functional_groups.txt', 'r') as f:
+    for line in f:
+        gid, smarts = line.split()
+        try:
+            pattern = Chem.MolFromSmarts(smarts)
+        except Exception:
+            continue
+        groups[smarts] = pattern
 
 # Initialize connection with the database.
 try:
@@ -41,43 +54,91 @@ for rec in db['retro'].find():
     transforms.append(t)
 
 # Get a random sample of chemical compounds.
-samplesize = 1000
-sample = Sample(db['chemical'])
+samplesize = 100
+sample = Sample(db['chemical'], rng_seed=1)
 
-# For each chemical perform a single retrosynthetic step.
+# For each chemical in the sample perform a single retrosynthetic step.
 reactions = {}
 for rec in sample.get(samplesize):
-    chem = Chemical(rec['smiles'].encode('ascii'))
+    smi = rec['smiles'].encode('ascii')
+    chem = Chemical(smi)
 
     # Add existing incoming reactions of the chemical to the pool of all
-    # possible reactions allowing for sythnesizing the compound.
+    # possible reactions allowing for synthesizing the compound.
     for uid in rec['reactions']['produced']:
         smi = db['reaction'].find_one({'_id': uid}).get('smiles', None)
         if smi is not None:
-            reactions[smi.encode('ascii')] = 1
+            smi = smi.encode('ascii')
+            reactions[smi] = Reaction(smi)
+            reactions[smi].is_published = True
 
     # Then, iterate over available transforms to generate any possible reaction
-    # leading to it, adding only unkonwn new ones to the pool.
-    #
-    # Note:
-    # Using setdefault() method enusres that existing reactions, already in the
-    # pool, will not their status overwritten.
-    for transform in transforms:
-        if len(transform.retrons) != 1:
-            continue
-        for smi in chem.make_retrostep(transform):
-            reactions.setdefault(smi, 0)
+    # leading to it, adding only unknown new ones to the pool.
+    for t in [t for t in transforms if len(t.retrons) == 1]:
+        for smi in chem.make_retrostep(t):
+            if smi not in reactions:
+                reactions[smi] = Reaction(smi)
+                reactions[smi].is_published = False
+
+# Find functional groups which are present on chemicals involved in each
+# reaction, either from the database or finding them from a scratch.
+field = 'functional_groups'
+bad_smis = []
+for smi, rxn in reactions.items():
+    chemicals = rxn.reactants + rxn.products
+
+    if None in [chem.mol for chem in chemicals]:
+        bad_smis.append(smi)
+        continue
+
+    for chem in chemicals:
+        rec = db.chemical.find_one({'smiles': chem.smiles})
+        if rec is not None and field in rec.keys():
+            chem.functional_groups = rec[field]
+        else:
+            chem.find_groups(groups)
+
+# For now, remove any reaction which has None among it reactants or products.
+for smi in bad_smis:
+    del reactions[smi]
 
 # Calculate reactions descriptors.
 indata = []
 status = []
-for smi, stat in reactions.items():
-    rxn = Reaction(smi)
-    indata.append(rxn.get_descriptors())
-    status.append([stat])
+for smi, rxn in reactions.items():
+    #indata.append(rxn.get_descriptors())
+    indata.append(rxn.get_group_descriptor(groups))
+    status.append([int(rxn.is_published)])
 save_array(indata, 'descriptors.dat')
 save_array(status, 'status.dat')
 
-# Here goes PCA analysis.
-outdata = mdp.pca(array(indata))
+
+# Here goes PCA analysis with help of Modular toolkit for Data Processing
+# (MDP):
+#     http://mdp-toolkit.sourceforge.net
+#
+# A node is the basic building block of an MDP application. It represents a
+# data processing element, here node performs PCA.
+#
+# Number of principal components which will be kept can be limited by
+# 'output_dim' parameter (default: None), e.g.:
+#  - output_dim=10 will keep first 10 principal components
+#  - output_dim=0.8 will keep the number of principal components which can
+#    account for 80% of the input variance.
+pcanode = mdp.nodes.PCANode(output_dim=0.9)
+
+# The node need to be trained to perform its task. PCA algorithm requires the
+# computation of the mean and covariance matrix of a set of training data from
+# which the principal eigenvectors of the data distribution are estimated.
+pcanode.train(array(indata))
+
+# To make PCA algorithm to compute the selected principal eigenvectors,
+# training must be finalized.
+pcanode.stop_training()
+
+# Once the training is finished, it is possible to execute the node, i.e. to
+# project the input data on the principal components learned in the training
+# phase.
+outdata = pcanode.execute(array(indata))
+
 save_array(outdata, 'pca.dat')
