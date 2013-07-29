fd3df8c3dfbda9e3323c9208a1ae0bc56a68d516
Mikolaj Kowalik <mxk62@psu.edu>
2013-07-29 18:23:34 -0400

    Add group storing/retrieving ability to Chemical
    Previously Chemical class was not able to store information about functional
    groups present on a compound for which it was instantiated. I added data
    attribute and method allowing it for storing and finding, if necessary,
    those groups.

--- src/chemical.py
+++ src/chemical.py
@@ -12,15 +12,16 @@ class Chemical:
         except Exception:
             print 'Error: invalid compound SMILES: {}.'.format(self.smiles)
             sys.exit(1)
+        self.functional_groups = None
 
     def make_retrostep(self, transform):
-        """Returns unique reactions obtained by retrosynthesis."""
+        """Returns unique reaction smiles obtained by retrosynthesis."""
 
         product_list = []
         try:
             product_list = transform.formula.RunReactants((self.mol,))
         except Exception:
-            # For now, ignore silently any errors
+            # For now, ignore silently any errors.
             pass
 
         rxn_smis = set([])
@@ -32,6 +33,32 @@ class Chemical:
             rxn_smis.add(reactant_smis + '>>' + self.smiles)
         return rxn_smis
 
+    def find_groups(self, groups):
+        """Finds which functional groups are present on the chemical.
+
+        Argument 'groups' is expected to be a dictionary which keys are
+        functional gropus SMARTS and values are RDKit's query mols representing
+        a gienb group.
+        """
+        self.functional_groups = {}
+        for smarts, pattern in groups.items():
+            if self.mol.HasSubstructMatch(pattern):
+                self.functional_groups.setdefault(smarts, 0)
+                self.functional_groups[smarts] += \
+                    len(self.mol.GetSubstructMatches(pattern))
+
 
 if __name__ == '__main__':
-    pass
+    groups = {}
+    with open('functional_groups.txt') as f:
+        for line in f:
+            gid, smarts = line.split()
+            try:
+                pattern = Chem.MolFromSmarts(smarts)
+            except Exception:
+                continue
+            groups[smarts] = pattern
+
+    chem = Chemical('NC(=O)c1ccc(C(N)=O)cc1')
+    chem.find_groups(groups)
+    print chem.smiles, chem.functional_groups
